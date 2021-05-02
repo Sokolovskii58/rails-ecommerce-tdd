6d331bcd267104b21af1c0fdf17728686fd7128e
Sokolovskii58 <arenda244@ya.ru>
2021-05-02 23:44:53 +0400

    Meta Tags

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,14 @@
 class MainController < ApplicationController
+  before_action :set_page_options
 
   def index
     @brands = Brand.limit(3)
     @hits = Product.all.limit(8)
+  end
+
+  def set_page_options
+    @page_title = 'Sneakers Shop'
+    @page_description = 'Sneakers'
+    @page_keywords = 'sneakers high heels shoe stores vans shoes mens shoes'
   end
 end
