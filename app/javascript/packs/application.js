ebf0fee1a718b74d4e274b4d92f53d3f45a34a87
Sokolovskii58 <arenda244@ya.ru>
2021-05-03 21:21:43 +0400

    test template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,27 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap")
 
+require("../js/vendor/bootstrap.bundle.min.js")
+
+require("../js/vendor/jquery-3.5.1.min.js")
+ 
+require("../js/vendor/bootstrap.bundle.min.js")
+require("../js/vendor/jquery-3.5.1.min.js")
+require("../js/vendor/jquery-migrate-3.3.0.min.js")
+require("../js/vendor/modernizr-3.11.2.min.js")
+require("../js/plugins/countdown.min.js")
+require("../js/plugins/aos.min.js")
+require("../js/plugins/swiper-bundle.min.js")
+require("../js/plugins/nice-select.min.js")
+require("../js/plugins/jquery.ajaxchimp.min.js")
+require("../js/plugins/jquery-ui.min.js")
+require("../js/plugins/lightgallery-all.min.js")
+require("../js/plugins/thia-sticky-sidebar.min.js")
+require("../js/vendor.min.js")
+require("../js/plugins.min.js")
+
+require("../js/main.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ProductController, type: :controller do
     describe 'GET #index' do
         let(:product) { create :product }
 
-        subject { get :show, params : { id: product.id } }
+        subject { get :show, params: { id: product.id } }
 
         context 'show product' do
             it 'render render show view' do
