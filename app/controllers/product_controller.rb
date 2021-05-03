799b6f3070d76e9e32b2a0d4399715f1382bd215
Sokolovskii58 <arenda244@ya.ru>
2021-05-03 23:22:58 +0400

    Recent Product

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,24 @@
 class ProductController < ApplicationController
 
   def show
-
+    @product = Product.find(params[:id])
+    set_page_options
   end
+
+  def recent_products
+    [] if recently.none?
+    Product.where(id: recently)
+  end
+
+  def recently
+    session[:viewed_products] || = []
+  end
+
+  
+  attr_accessor :product
+
+  def set_page_options
+    set_meta_tags product.slice(:title, :keywords, :description)
+  end
+
 end
