60c2a472e704698f564c72ec9422357c0ef49aaf
Sokolovskii58 <arenda244@ya.ru>
2021-05-04 17:36:50 +0400

    Gallery Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
 
+  has_many :galleries
+
   has_many :related_products
   has_many :related, through: :related_products
-  
+
   enum hit: { hit: 1, not_hit: 0 }
 end
--- db/migrate/20210504132733_create_galleries.rb
+++ db/migrate/20210504132733_create_galleries.rb
@@ -1,8 +1,18 @@
 class CreateGalleries < ActiveRecord::Migration[6.0]
   def change
     create_table :galleries do |t|
+      t.integer :product_id
+      t.string :img
+     end
 
-      t.timestamps
-    end
+     insert <<-SQL.squish
+             INSERT INTO galleries
+             (id , product_id , img)
+             VALUES
+             (1, 2, 's-1.jpg'),
+             (2, 2, 's-2.jpg'),
+             (3, 3, 's-3.jpg')
+
+     SQL
   end
 end
