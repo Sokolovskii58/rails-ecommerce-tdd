5c572513d47eb927d9df536d1f090ad5db9621c9
Sokolovskii58 <arenda244@ya.ru>
2021-05-05 17:57:04 +0400

    Model Cart_Item

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_133050) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
-    t.integer "product"
+    t.integer "product_id"
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:product)}
+  it { should belong_to(:cart) }
+  it { should validate_presence_of(:cart_id)}
+  it { should validate_presence_of(:quantity)}
+
 end
